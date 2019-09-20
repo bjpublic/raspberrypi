6576792a3453c14f01f978e4d254cd372273ebfa
BJPUBLIC <bjpublic@bjpublic.co.kr>
2019-09-20 11:10:38 +0900

    Update case_test.sh

--- chapter03/case_test.sh
+++ chapter03/case_test.sh
@@ -5,16 +5,16 @@ for string in "HELLO" "BASH" "hello" "bash" "star" "start" "end" "finish"; do
     # case문 시작
     case ${string} in
         hello|HELLO)
-            echo "${string}: hello 일때 실행하는 문장이다.""
+            echo "${string}: hello 일때 실행하는 문장이다."
             ;;
         hard*)
-            echo "${string}: hard로 시작하는 단어 일때 실행하는 문장이다.""
+            echo "${string}: hard로 시작하는 단어 일때 실행하는 문장이다."
             ;;
         star|start)
             echo "${string}: star 혹은 start 일때 실행하는 문장이다."
             ;;
         finish|end)
-            echo "${string}: finish 혹은 end 일때 실행하는 문장이다.""
+            echo "${string}: finish 혹은 end 일때 실행하는 문장이다."
             ;;
         *)
             echo "${string}: 기타"
